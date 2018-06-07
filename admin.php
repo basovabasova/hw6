bfac13bc94542b7a8cd591a7761d82189e4cb9b7
basovabasova <fedosova.olya@gmail.com>
2018-06-07 23:58:38 +0300

    lesson6

---
+++ admin.php
@@ -0,0 +1,33 @@
+<?php
+    foreach ($_FILES as $key => $value) {
+        if (isset($_POST) && isset($_FILES)) {
+            $file = $value['name'];
+            $tmp = $value['tmp_name'];
+            $uploaddir = 'tests/';
+            $path_info = pathinfo($uploaddir . $file);
+
+            if ($path_info['extension'] === 'json') {
+                move_uploaded_file($tmp, $uploaddir . $file);
+                $a = "Файл передан" . '<br>';
+            } else {
+                $a = "Файл не передан, нужен файл json." . '<br>';
+            }
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>Загрузить тесты</title>
+</head>
+<body>
+  <form method="post" enctype="multipart/form-data" name="myform">
+    <legend>Загрузить тесты</legend>
+      <label><input type="file" name="usefile1"><?php echo $a;?></label>
+      <label><input type="file" name="usefile"><?php echo $a;?></label>
+    <input type="submit" value="Отправить"><br>
+  <p><a href="list.php">Выбрать тест</a></p>
+</body>
+</html>
